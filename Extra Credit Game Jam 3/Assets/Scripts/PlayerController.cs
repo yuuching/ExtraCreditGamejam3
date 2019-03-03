17275b92c84db00a5092ad3a86d1c8670e1e8fc3
Yuuchi Ng <chiuyenngo@yahoo.com>
2019-03-03 02:23:10 -0800

    Animation and Walk cycle works now Crying emoji

---
+++ Extra Credit Game Jam 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed = 10.0f;
+    private float movement = 0f;
+
+    public Animator animator;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+        Debug.Log(Speed);
+    }
+
+    void FixedUpdate()
+    {
+        float movement = Input.GetAxis("Horizontal");
+
+        if (movement > 0.0f)
+        {
+            rb.velocity = new Vector2(movement * Speed, rb.velocity.y);
+        }
+
+        if (movement == 0f)
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+           
+        }
+
+        else
+        {
+            rb.velocity = new Vector2(movement * Speed, rb.velocity.y);
+        }
+
+        animator.SetFloat("Speed", movement);
+
+    }
+}
